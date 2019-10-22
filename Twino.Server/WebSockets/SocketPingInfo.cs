5f1c0987fbf3229f9998100d3e1e7219123bf19a
Mehmet Helvacıköylü <mehmet.helvacikoylu@btctrader.com>
2019-10-22 15:54:54 +0300

    websocket pinger time bug fix

--- Twino.Server/WebSockets/SocketPingInfo.cs
+++ Twino.Server/WebSockets/SocketPingInfo.cs
@@ -5,27 +5,27 @@ namespace Twino.Server.WebSockets
     /// <summary>
     /// Ping meta data for the web socket connection
     /// </summary>
-    internal struct SocketPingInfo
+    internal class SocketPingInfo
     {
         /// <summary>
         /// The client
         /// </summary>
-        public ServerSocket Socket;
+        public ServerSocket Socket { get; }
         
         /// <summary>
         /// Last ping time
         /// </summary>
-        public DateTime Update;
+        public DateTime Last { get; private set; }
         
         /// <summary>
         /// If true, first ping did not send
         /// </summary>
-        public bool New;
-
+        public bool New { get; private set; }
+        
         public SocketPingInfo(ServerSocket socket)
         {
             Socket = socket;
-            Update = DateTime.UtcNow;
+            Last = DateTime.UtcNow;
             New = true;
         }
 
@@ -34,8 +34,8 @@ namespace Twino.Server.WebSockets
         /// </summary>
         public void Ping()
         {
-            Update = DateTime.UtcNow;
-
+            Last = DateTime.UtcNow;
+            
             if (New)
                 New = false;
 
