7a15c2c20ae1ce5413b7a77807570ec8228036c7
Mehmet Helvacıköylü <mehmet.helvacikoylu@btctrader.com>
2019-11-08 17:26:35 +0300

    ioc, transient pool test

--- Tests/Test.Ioc/PoolTest.cs
+++ Tests/Test.Ioc/PoolTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading.Tasks;
 using Test.Ioc.Services;
 using Twino.Ioc;
 using Xunit;
@@ -7,23 +9,54 @@ namespace Test.Ioc
     public class PoolTest
     {
         [Fact]
-        public void Basic()
+        public async Task Transient()
         {
+            ServiceContainer services = new ServiceContainer();
+            services.AddTransientPool<ISingleService, SingleService>();
+
+            IContainerScope scope = services.CreateScope();
+
+            ISingleService single = await services.Get<ISingleService>(scope);
+            single.Foo = "single";
+
+            ISingleService s1 = await services.Get<ISingleService>(scope);
+            Assert.NotEqual(single.Foo, s1.Foo);
+            
+            ISingleService s2 = await services.Get<ISingleService>();
+            Assert.NotEqual(single.Foo, s2.Foo);
+
+            IContainerScope scope2 = services.CreateScope();
+            ISingleService s3 = await services.Get<ISingleService>(scope2);
+            Assert.NotEqual(single.Foo, s3.Foo);
         }
 
         [Fact]
-        public void LongRunning()
+        public async Task Scoped()
         {
+            ServiceContainer services = new ServiceContainer();
+            services.AddScopedPool<ISingleService, SingleService>();
+
+            IContainerScope scope = services.CreateScope();
+
+            throw new NotImplementedException();
         }
 
         [Fact]
-        public void WaitLimitAndGet()
+        public async Task LongRunning()
         {
+            throw new NotImplementedException();
         }
 
         [Fact]
-        public void WaitLimitAndTimeout()
+        public async Task WaitLimitAndGet()
         {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task WaitLimitAndTimeout()
+        {
+            throw new NotImplementedException();
         }
     }
 }
