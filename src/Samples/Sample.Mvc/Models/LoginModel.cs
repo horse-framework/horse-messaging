0acc17ff2ac41f22b2088826f0605952ff6ff9b4
Mehmet Helvacıköylü <mehmet.helvacikoylu@btctrader.com>
2019-12-16 15:31:13 +0300

    protocol read fixes

--- src/Samples/Sample.Mvc/Models/LoginModel.cs
+++ src/Samples/Sample.Mvc/Models/LoginModel.cs
@@ -1,4 +1,3 @@
-using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 
 namespace Sample.Mvc.Models
@@ -6,11 +5,9 @@ namespace Sample.Mvc.Models
     public class LoginModel
     {
         [JsonProperty("username")]
-        [JsonPropertyName("username")]
         public string Username { get; set; }
 
         [JsonProperty("password")]
-        [JsonPropertyName("password")]
         public string Password { get; set; }
     }
 }
