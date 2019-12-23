9bf261d4917fcaa67a555f8e1f9579200836957e
Mehmet Helvacıköylü <mehmet.helvacikoylu@btctrader.com>
2019-12-23 19:03:45 +0300

    release 2.1

--- src/Twino.SerializableModel/obj/Release/netcoreapp3.0/Twino.SerializableModel.AssemblyInfo.cs
+++ src/Twino.SerializableModel/obj/Release/netcoreapp3.0/Twino.SerializableModel.AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Twino Serializable Model library for object based communication with text-based p" +
 "rotocols")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.30")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("2.1.2")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Twino.SerializableModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Twino.SerializableModel")]
-[assembly: System.Reflection.AssemblyVersionAttribute("2.0.30")]
+[assembly: System.Reflection.AssemblyVersionAttribute("2.1.2")]
