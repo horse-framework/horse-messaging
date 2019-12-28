8782cbd5c513df8b28f06f9d10853e199b8d3e4f
Mehmet Helvacıköylü <mhelvacikoylu@gmail.com>
2019-12-28 22:41:18 +0300

    auto channel and queue creation

--- src/Twino.MQ/Network/ChannelMessageHandler.cs
+++ src/Twino.MQ/Network/ChannelMessageHandler.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 using Twino.MQ.Clients;
 using Twino.MQ.Helpers;
 using Twino.MQ.Queues;
@@ -26,6 +27,11 @@ namespace Twino.MQ.Network
         {
             //find channel and queue
             Channel channel = _server.FindChannel(message.Target);
+
+            //if auto creation active, try to create channel
+            if (channel == null && _server.Options.AutoChannelCreation)
+                channel = _server.CreateChannel(message.Target);
+
             if (channel == null)
             {
                 if (!string.IsNullOrEmpty(message.MessageId))
@@ -34,6 +40,11 @@ namespace Twino.MQ.Network
             }
 
             ChannelQueue queue = channel.FindQueue(message.ContentType);
+
+            //if auto creation active, try to create queue
+            if (queue == null && _server.Options.AutoQueueCreation)
+                queue = await channel.CreateQueue(message.ContentType);
+
             if (queue == null)
             {
                 if (!string.IsNullOrEmpty(message.MessageId))
