cfe43cd42205f4c81e0a8bc65aa82d1b30868170
Mehmet Helvacıköylü <mehmet.helvacikoylu@btctrader.com>
2020-01-15 10:38:56 +0300

    channel client count added, trigger is skipped if there are no clients

--- src/Twino.MQ/Queues/ChannelQueue.cs
+++ src/Twino.MQ/Queues/ChannelQueue.cs
@@ -619,6 +619,9 @@ namespace Twino.MQ.Queues
         /// </summary>
         public async Task Trigger()
         {
+            if (Channel.ClientsCount() == 0)
+                return;
+            
             if (_triggering)
                 return;
 
@@ -790,7 +793,7 @@ namespace Twino.MQ.Queues
             //after all sending operations completed, calls implementation send completed method and complete the operation
             if (messageIsSent)
                 Info.AddMessageSend();
-            
+
             message.Decision = await DeliveryHandler.EndSend(this, message);
             await ApplyDecision(message.Decision, message);
 
@@ -814,16 +817,16 @@ namespace Twino.MQ.Queues
 
             if (decision.Allow)
                 allow = true;
-            
+
             if (decision.KeepMessage)
                 keep = true;
-            
+
             if (decision.SaveMessage)
                 save = true;
-            
+
             if (decision.SendAcknowledge == DeliveryAcknowledgeDecision.Always)
                 ack = DeliveryAcknowledgeDecision.Always;
-            
+
             else if (decision.SendAcknowledge == DeliveryAcknowledgeDecision.IfSaved && final.SendAcknowledge == DeliveryAcknowledgeDecision.None)
                 ack = DeliveryAcknowledgeDecision.IfSaved;
 
