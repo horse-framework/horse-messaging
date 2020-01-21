02d65aaa35fae30f07e73be4dfda6e52e90813a2
Mehmet Helvacıköylü <mhelvacikoylu@gmail.com>
2020-01-22 00:27:12 +0300

    refactor

--- src/Twino.MQ/Delivery/Decision.cs
+++ src/Twino.MQ/Delivery/Decision.cs
@@ -1,27 +1,6 @@
 namespace Twino.MQ.Delivery
 {
     /// <summary>
-    /// Sendin Acknowledge decision for delivery decision
-    /// </summary>
-    public enum DeliveryAcknowledgeDecision : byte
-    {
-        /// <summary>
-        /// Acknowledge message is not sent
-        /// </summary>
-        None,
-
-        /// <summary>
-        /// Acknowledge message is sent, even message is not saved or failed 
-        /// </summary>
-        Always,
-
-        /// <summary>
-        /// Acknowledge message is sent only if message save is successful
-        /// </summary>
-        IfSaved
-    }
-
-    /// <summary>
     /// Decision description for each step in message delivery
     /// </summary>
     public struct Decision
---
+++ src/Twino.MQ/Delivery/DeliveryAcknowledgeDecision.cs
@@ -0,0 +1,23 @@
+namespace Twino.MQ.Delivery
+{
+    /// <summary>
+    /// Sendin Acknowledge decision for delivery decision
+    /// </summary>
+    public enum DeliveryAcknowledgeDecision : byte
+    {
+        /// <summary>
+        /// Acknowledge message is not sent
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Acknowledge message is sent, even message is not saved or failed 
+        /// </summary>
+        Always,
+
+        /// <summary>
+        /// Acknowledge message is sent only if message save is successful
+        /// </summary>
+        IfSaved
+    }
+}
