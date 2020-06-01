978309c0e1cf9b2483792c436a42358ebb9c9499
Mehmet Helvacıköylü <mehmet.helvacikoylu@btctrader.com>
2020-06-01 18:02:49 +0300

    sample projects

--- src/Samples/Playground/Program.cs
+++ src/Samples/Playground/Program.cs
@@ -3,8 +3,13 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Test.Mq.Internal;
+using Test.Mq.Models;
+using Twino.Client.TMQ;
 using Twino.Core.Protocols;
+using Twino.MQ;
 using Twino.MQ.Data;
+using Twino.MQ.Options;
 using Twino.Mvc;
 using Twino.Mvc.Controllers;
 using Twino.Mvc.Controllers.Parameters;
@@ -20,6 +25,10 @@ namespace Playground
     {
         static async Task Main(string[] args)
         {
+            TestMqServer server = new TestMqServer();
+            server.Initialize(84);
+            server.Start(10000, 10000);
+            /*
             TwinoServer _server = new TwinoServer();
             _server = new TwinoServer(ServerOptions.CreateDefault());
             _server.UseWebSockets(async (socket) => { await socket.SendAsync("Welcome"); },
@@ -28,8 +37,8 @@ namespace Playground
                                       Console.WriteLine("# " + message);
                                       await socket.SendAsync(message);
                                   });
-            _server.Start(46100);
-            _server.BlockWhileRunning();
+            _server.Start(46100);*/
+            server.Server.Server.BlockWhileRunning();
         }
     }
 }
