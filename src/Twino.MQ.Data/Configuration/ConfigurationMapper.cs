e9515218dda10c3489ee16a95439e47a1f372228
Mehmet Helvacıköylü <mhelvacikoylu@gmail.com>
2020-07-29 15:55:47 +0300

    data configuration mapping

--- src/Twino.MQ.Data/Configuration/ConfigurationMapper.cs
+++ src/Twino.MQ.Data/Configuration/ConfigurationMapper.cs
@@ -1,5 +1,6 @@
 using System;
 using Twino.MQ.Options;
+using Twino.MQ.Queues;
 
 namespace Twino.MQ.Data.Configuration
 {
@@ -7,22 +8,103 @@ namespace Twino.MQ.Data.Configuration
     {
         internal static ChannelOptions ToOptions(this ChannelOptionsConfiguration configuration)
         {
-            throw new NotImplementedException();
+            return new ChannelOptions
+                   {
+                       Status = configuration.Status.ToQueueStatus(),
+                       AcknowledgeTimeout = TimeSpan.FromMilliseconds(configuration.AcknowledgeTimeout),
+                       MessageTimeout = TimeSpan.FromMilliseconds(configuration.AcknowledgeTimeout),
+                       AllowedQueues = configuration.AllowedQueues,
+                       ClientLimit = configuration.ClientLimit,
+                       MessageLimit = configuration.MessageLimit,
+                       QueueLimit = configuration.QueueLimit,
+                       RequestAcknowledge = configuration.RequestAcknowledge,
+                       TagName = configuration.TagName,
+                       AllowMultipleQueues = configuration.AllowMultipleQueues,
+                       DestroyWhenEmpty = configuration.DestroyWhenEmpty,
+                       HideClientNames = configuration.HideClientNames,
+                       MessageSizeLimit = configuration.MessageSizeLimit,
+                       UseMessageId = configuration.UseMessageId,
+                       WaitForAcknowledge = configuration.WaitForAcknowledge,
+                       SendOnlyFirstAcquirer = configuration.SendOnlyFirstAcquirer
+                   };
         }
 
         internal static ChannelQueueOptions ToOptions(this QueueOptionsConfiguration configuration)
         {
-            throw new NotImplementedException();
+            return new ChannelQueueOptions
+                   {
+                       Status = configuration.Status.ToQueueStatus(),
+                       AcknowledgeTimeout = TimeSpan.FromMilliseconds(configuration.AcknowledgeTimeout),
+                       MessageTimeout = TimeSpan.FromMilliseconds(configuration.AcknowledgeTimeout),
+                       MessageLimit = configuration.MessageLimit,
+                       RequestAcknowledge = configuration.RequestAcknowledge,
+                       TagName = configuration.TagName,
+                       HideClientNames = configuration.HideClientNames,
+                       MessageSizeLimit = configuration.MessageSizeLimit,
+                       UseMessageId = configuration.UseMessageId,
+                       WaitForAcknowledge = configuration.WaitForAcknowledge,
+                       SendOnlyFirstAcquirer = configuration.SendOnlyFirstAcquirer
+                   };
         }
 
         internal static ChannelOptionsConfiguration ToConfiguration(this ChannelOptions options)
         {
-            throw new NotImplementedException();
+            return new ChannelOptionsConfiguration
+                   {
+                       Status = options.Status.ToStatusString(),
+                       AcknowledgeTimeout = Convert.ToInt32(options.AcknowledgeTimeout.TotalMilliseconds),
+                       AllowedQueues = options.AllowedQueues,
+                       ClientLimit = options.ClientLimit,
+                       MessageLimit = options.MessageLimit,
+                       MessageTimeout = Convert.ToInt32(options.MessageTimeout.TotalMilliseconds),
+                       QueueLimit = options.QueueLimit,
+                       RequestAcknowledge = options.RequestAcknowledge,
+                       TagName = options.TagName,
+                       AllowMultipleQueues = options.AllowMultipleQueues,
+                       DestroyWhenEmpty = options.DestroyWhenEmpty,
+                       HideClientNames = options.HideClientNames,
+                       MessageSizeLimit = options.MessageSizeLimit,
+                       UseMessageId = options.UseMessageId,
+                       WaitForAcknowledge = options.WaitForAcknowledge,
+                       SendOnlyFirstAcquirer = options.SendOnlyFirstAcquirer
+                   };
         }
 
         internal static QueueOptionsConfiguration ToConfiguration(this ChannelQueueOptions options)
         {
-            throw new NotImplementedException();
+            return new QueueOptionsConfiguration
+                   {
+                       Status = options.Status.ToStatusString(),
+                       AcknowledgeTimeout = Convert.ToInt32(options.AcknowledgeTimeout.TotalMilliseconds),
+                       MessageLimit = options.MessageLimit,
+                       MessageTimeout = Convert.ToInt32(options.MessageTimeout.TotalMilliseconds),
+                       RequestAcknowledge = options.RequestAcknowledge,
+                       TagName = options.TagName,
+                       HideClientNames = options.HideClientNames,
+                       MessageSizeLimit = options.MessageSizeLimit,
+                       UseMessageId = options.UseMessageId,
+                       WaitForAcknowledge = options.WaitForAcknowledge,
+                       SendOnlyFirstAcquirer = options.SendOnlyFirstAcquirer
+                   };
+        }
+
+        private static string ToStatusString(this QueueStatus status)
+        {
+            return status.ToString().ToLowerInvariant();
+        }
+
+        private static QueueStatus ToQueueStatus(this string text)
+        {
+            switch (text.ToLowerInvariant())
+            {
+                case "broadcast": return QueueStatus.Broadcast;
+                case "push": return QueueStatus.Push;
+                case "roundrobin": return QueueStatus.RoundRobin;
+                case "pull": return QueueStatus.Pull;
+                case "cache": return QueueStatus.Cache;
+                case "paused": return QueueStatus.Paused;
+                default: return QueueStatus.Stopped;
+            }
         }
     }
 }
