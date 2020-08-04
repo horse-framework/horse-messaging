77fee75c857ebc54c4ac16d595341a9b143f9f4b
Emre Hızlı <hizliemre@gmail.com>
2020-08-04 19:24:50 +0300

    fix pushjson bug in RouterOperator.cs

--- src/Samples/Sample.Producer/Models/Model C.cs
+++ src/Samples/Sample.Producer/Models/Model C.cs
@@ -3,7 +3,7 @@ using Twino.Client.TMQ.Annotations;
 namespace Sample.Producer.Models
 {
     [ContentType(5)]
-    [DirectTarget(FindTargetBy.Name, "consumer")]
+    [RouterName("deneme-router")]
     public class ModelC
     {
         public string Value { get; set; }
