ea341b4767b450f06b5eebdd95680fc0b2035ba1
Mehmet Helvacıköylü <mhelvacikoylu@gmail.com>
2020-08-09 23:53:56 +0300

    queue status with push model attributes

--- src/Twino.Client.TMQ/Annotations/AutoNackAttribute.cs
+++ src/Twino.Client.TMQ/Annotations/AutoNackAttribute.cs
@@ -3,32 +3,6 @@ using System;
 namespace Twino.Client.TMQ.Annotations
 {
     /// <summary>
-    /// Negative Acknowledge reasons
-    /// </summary>
-    public enum NackReason
-    {
-        /// <summary>
-        /// Reason none
-        /// </summary>
-        None,
-
-        /// <summary>
-        /// Reason error
-        /// </summary>
-        Error,
-
-        /// <summary>
-        /// Reason is class name of the exception
-        /// </summary>
-        ExceptionType,
-
-        /// <summary>
-        /// Reason is messge of the exception
-        /// </summary>
-        ExceptionMessage
-    }
-
-    /// <summary>
     /// Used on Consumer Interfaces.
     /// Sends negative acknowledge for the message if it's required after consume operation throws an exception
     /// </summary>
