2de7f732dd5369e91eae6cf277cf675de92f0026
Emre Hızlı <hizliemre@gmail.com>
2020-08-11 23:32:51 +0300

    base class handler

--- src/Samples/Sample.Consumer/Consumers/CRequestHandler.cs
+++ src/Samples/Sample.Consumer/Consumers/CRequestHandler.cs
@@ -2,22 +2,34 @@ using System;
 using System.Threading.Tasks;
 using Sample.Consumer.Models;
 using Twino.Client.TMQ;
+using Twino.Client.TMQ.Annotations;
 using Twino.Protocols.TMQ;
 
 namespace Sample.Consumer.Consumers
 {
-    public class CRequestHandler : ITwinoRequestHandler<ModelC, ModelA>
-    {
-        public async Task<ModelA> Handle(ModelC request, TmqMessage rawMessage, TmqClient client)
-        {
-            Console.WriteLine("Model C consumed");
-            await Task.CompletedTask;
-            return null;
-        }
-    
-        public async Task<ErrorResponse> OnError(Exception exception, ModelC request, TmqMessage rawMessage, TmqClient client)
-        {
-            throw new NotImplementedException();
-        }
-    }
+	[PushExceptions("error", 501)]
+	public class CRequestHandler: DenemeBase<ModelC, ModelA>
+	{
+		protected override Task<ModelA> Handle(ModelC request)
+		{
+			Console.WriteLine("Model C consumed");
+			throw new Exception("aaaa");
+		}
+	}
+
+	public abstract class DenemeBase<T, T1>: ITwinoRequestHandler<T, T1>
+	{
+		public Task<T1> Handle(T request, TmqMessage rawMessage, TmqClient client)
+		{
+			throw new Exception("asdasdasdasd");
+			return Handle(request);
+		}
+
+		public Task<ErrorResponse> OnError(Exception exception, T request, TmqMessage rawMessage, TmqClient client)
+		{
+			return Task.FromResult(new ErrorResponse());
+		}
+
+		protected abstract Task<T1> Handle(T request);
+	}
 }
