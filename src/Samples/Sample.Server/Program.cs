9885bc4283769249cd977c09a5d6536a2aff140c
Mehmet Helvacıköylü <mhelvacikoylu@gmail.com>
2020-09-02 23:04:59 +0300

    new sample projects

---
+++ src/Samples/Sample.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
