daa7913397cdc51a2618881a0f2d63497ecc60b1
Mehmet Helvacıköylü <mhelvacikoylu@gmail.com>
2020-09-04 00:01:13 +0300

    default sample queue status

--- src/Samples/Sample.Server/Program.cs
+++ src/Samples/Sample.Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Twino.MQ;
+using Twino.MQ.Queues;
 using Twino.Server;
 
 namespace Sample.Server
@@ -9,11 +10,12 @@ namespace Sample.Server
         static Task Main(string[] args)
         {
             TwinoMQ mq = TwinoMqBuilder.Create()
+                                       .AddOptions(o => o.Status = QueueStatus.Broadcast)
                                        .AddClientHandler<ClientHandler>()
                                        .AddQueueEventHandler<QueueEventHandler>()
                                        .UseJustAllowDeliveryHandler()
                                        .Build();
-            
+
             TwinoServer server = new TwinoServer();
             server.UseTwinoMQ(mq);
             server.Start(26222);
