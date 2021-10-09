0420f5c9e851fee48dfeaa48f5f096fde7f436d3
Emre Hızlı <hizliemre@gmail.com>
2021-10-09 14:30:01 +0300

    fix settings sample

--- src/Samples/HostedServiceSample.Consumer/Program.cs
+++ src/Samples/HostedServiceSample.Consumer/Program.cs
@@ -3,5 +3,6 @@ using HostedServiceSample.Producer;
 
 var service = HorseServiceFactory.Create<Program>(args);
 service.ConfigureHorseClient(clientBuilder => clientBuilder.AddTransientConsumer<TestQueueModelConsumer>());
+service.Run();
 
 internal class Program { }
