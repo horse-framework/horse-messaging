489430e1abbaf35c695c37bb1b96353d898e962f
Emre Hızlı <hizliemre@gmail.com>
2021-10-09 14:33:41 +0300

    hosted sample fix

--- src/Samples/HostedServiceSample.Consumer/Program.cs
+++ src/Samples/HostedServiceSample.Consumer/Program.cs
@@ -2,6 +2,7 @@
 using HostedServiceSample.Producer;
 
 var service = HorseServiceFactory.Create<Program>(args);
+service.ConfigureHorseClient(clientBuilder => clientBuilder.AddTransientConsumer<TestQueueModelConsumer>());
 service.Run();
 
 internal class Program { }
