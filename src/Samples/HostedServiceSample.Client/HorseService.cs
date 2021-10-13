f64d0f78a3ce3f3f2655ab014a77c28b97c80a48
Emre Hızlı <hizliemre@gmail.com>
2021-10-13 19:07:17 +0300

    comment

--- src/Samples/HostedServiceSample.Client/HorseService.cs
+++ src/Samples/HostedServiceSample.Client/HorseService.cs
@@ -3,6 +3,10 @@ namespace HostedServiceSample.Client
 	internal sealed class HorseService<T> : HorseServiceBase
 		where T : class
 	{
-		public HorseService(string[] args) : base(args) { }
+		public HorseService(string[] args) : base(args)
+		{
+			// You can use the below code to add your all consumers into the dependency injection container
+			// ConfigureHorseClient(builder => builder.AddScopedConsumers(typeof(T)));
+		}
 	}
 }
